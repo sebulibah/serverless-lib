73bc503b1bf3651f26eae6ca2a683d7b4a9771d0
sebulibah <sebuliba.hannah@live.com>
2021-04-21 20:06:14 +0300

    feat: add books business logic

---
+++ backend/src/businessLogic/books.ts
@@ -0,0 +1,66 @@
+import * as uuid from 'uuid'
+
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { BookItem } from '../models/BookItem'
+import { BookAccess } from '../dataLayer/bookAccess'
+import { CreateBookRequest } from '../requests/CreateBookRequest'
+import { UpdateBookRequest } from '../requests/UpdateBookRequest'
+import { getUserId } from '../lambda/utils'
+
+
+const bookAccess = new BookAccess()
+
+export async function createBook(
+    event: APIGatewayProxyEvent,
+    createBookRequest: CreateBookRequest): Promise<BookItem>{
+    const itemId = uuid.v4()
+    const userId = getUserId(event)
+
+    const book = {
+        userId,
+        bookId: itemId,
+        createdAt: new Date().toISOString(),
+        title: createBookRequest.title,
+        dueDate: createBookRequest.dueDate,
+        read: false
+    }
+    await bookAccess.createBook(book)
+    return book
+}
+
+export async function getAllBooks(event: APIGatewayProxyEvent): Promise<BookItem>{
+    const userId = getUserId(event)
+    return await bookAccess.getAllBooks(userId)
+}
+
+export async function updateBook(
+    bookId: string,
+    event: APIGatewayProxyEvent,
+    updateBookRequest: UpdateBookRequest): Promise<BookItem>{
+
+    const userId = getUserId(event)
+
+    return await bookAccess.updateBook({
+        userId,
+        bookId,
+        createdAt: new Date().toISOString(),
+        title: updateBookRequest.title,
+        dueDate: updateBookRequest.dueDate,
+        read: updateBookRequest.read
+    })
+}
+
+
+export async function deleteBook(
+    bookId: string,
+    event: APIGatewayProxyEvent
+    ): Promise<string>{
+
+    const userId = getUserId(event)
+    return await bookAccess.deleteBook(userId, bookId)
+
+}
+
+export async function generateUploadURL(bookId: string): Promise<string> {
+    return bookAccess.generateUploadUrl(bookId)
+}
